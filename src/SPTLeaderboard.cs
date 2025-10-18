2f26c09071ae748743c79acc6420b3ea65b86118
Yui <yui@osufx.com>
2025-10-18 13:50:06 -0300

    docs(metadata): added metadata

--- src/SPTLeaderboard.cs
+++ src/SPTLeaderboard.cs
@@ -9,19 +9,19 @@ namespace SPTLeaderboard;
 
 public record ModMetadata : AbstractModMetadata
 {
-    public override string ModGuid { get; init; } = "github.harmonyzt.SPTLeaderboard";
+    public override string ModGuid { get; init; } = "github.SPT-Leaderboard.Server";
     public override string Name { get; init; } = "SPTLeaderboard";
     public override string Author { get; init; } = "Harmony";
-    public override List<string>? Contributors { get; init; }
+    public override List<string>? Contributors { get; init; } = ["Katrin0522", "yuyui.moe"];
     public override SemanticVersioning.Version Version { get; init; } = new("1.0.0");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
 
 
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
-    public override string? Url { get; init; }
-    public override bool? IsBundleMod { get; init; }
-    public override string? License { get; init; } = "MIT";
+    public override string? Url { get; init; } = "https://sptlb.yuyui.moe";
+    public override bool? IsBundleMod { get; init; } = false;
+    public override string? License { get; init; } = "MPL 2.0";
 }
 
 [Injectable(TypePriority = OnLoadOrder.PreSptModLoader + 1)]
